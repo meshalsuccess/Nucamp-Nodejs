6d9cf20bdb682ef6edadd0d0c015fcf7c0dad5c9
meshalsuccess <meshalsuccess@gmail.com>
2021-01-18 13:41:50 -0800

    Adding Promotion's API after Davis's comment on the assignment

--- nucampsiteServer/routes/promotionRouter.js
+++ nucampsiteServer/routes/promotionRouter.js
@@ -37,4 +37,39 @@ promotionRouter.route('/')
   .catch(err => next(err));
 });
 
+promotionRouter.route('/:promotionId')
+.get((req, res, next) => {
+  promotion.findById(req.params.promotionId)
+  .then(promotion => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(promotion);
+  })
+  .catch(err => next(err));
+})
+.post((req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
+})
+.put((req, res, next) => {
+  promotion.findByIdAndUpdate(req.params.promotionId, {
+      $set: req.body
+  }, { new: true })
+  .then(promotion => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(promotion);
+  })
+  .catch(err => next(err));
+})
+.delete((req, res, next) => {
+  promotion.findByIdAndDelete(req.params.promotionId)
+  .then(response => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(response);
+  })
+  .catch(err => next(err));
+});
+
 module.exports = promotionRouter;
