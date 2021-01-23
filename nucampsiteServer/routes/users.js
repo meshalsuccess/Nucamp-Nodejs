c5f5405bdf417cb5df679428bae56622b40cdd25
meshalsuccess <meshalsuccess@gmail.com>
2021-01-23 05:56:19 -0800

    CORS

--- nucampsiteServer/routes/users.js
+++ nucampsiteServer/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const passport = require("passport");
 const authenticate = require("../authenticate");
+const cors = require('./cors');
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ const router = express.Router();
 //router.get : the GET method of users endpoint: verify the user , verify user is admin
 router.get(
   "/",
+  cors.corsWithOptions,
   authenticate.verifyUser,
   authenticate.verifyAdmin,
   (req, res, next) => {
@@ -24,7 +26,7 @@ router.get(
 );
 
 //endpoint allows new user to register on website
-router.post("/signup", (req, res) => {
+router.post("/signup", cors.corsWithOptions, (req, res) => {
 User.register(
     new User({ username: req.body.username }),
     req.body.password,
@@ -59,14 +61,14 @@ User.register(
   );
 });
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
+router.post("/login", cors.corsWithOptions, passport.authenticate("local"), (req, res) => {
   const token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.json({ success: true, token, status: "You are sucessfully logged in!" });
 });
 
-router.get("/logout", (req, res, next) => {
+router.get("/logout", cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie("session-id");
